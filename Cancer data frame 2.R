8e8addc20a129c3d21052db1d31cea81b441c2db
Nikitasoni1802 <tso.nikita@orbitbiotech.com>
2021-06-25 15:08:38 +0530

    Second commit

--- Cancer data frame 2.R
+++ Cancer data frame 2.R
@@ -12,13 +12,16 @@ dim(cancer)
 glimpse(cancer)
 cancer <- cancer[-33]
 summary(cancer)
-# no of women affected in begnin and malignant stage
+
+# no of women affected in benign and malignant stage
 cancer %>% count(diagnosis)
 
-# percentage of women affected in begnin and malignant stage
+# percentage of women affected in benign and malignant stage
 cancer %>% count(diagnosis)%>%group_by(diagnosis)%>%
-  summarise(perc_dx = round((n / 569)*100, 2))
+  summarise(perc_dx = round(n / 569*100,2))
+
 #data visualization
+
 # frequency of cancer diagnosis
 diagnosis.table <- table(cancer$diagnosis)
 colors <- terrain.colors(2)
@@ -27,36 +30,51 @@ colors <- terrain.colors(2)
 diagnosis.prop.table <- prop.table(diagnosis.table)*100
 diagnosis.prop.df <- as.data.frame(diagnosis.prop.table)
 pielables <- sprintf("%s - %3.1f%s", diagnosis.prop.df[,1], diagnosis.prop.table, "%")
-pie(diagnosis.prop.table,lables = pielables, clockwise = TRUE,col = colors,
-    border = "gainsboro",radius = 0.8,cex = 0.8, main = "frequency of cancer diagnosis")
-legend(1, .4, legend = diagnosis.prop.df[,1],cex = 0.7, fill = colors)
+pie(diagnosis.prop.table,lables = pietables,clockwise = TRUE,col = c("purple","red"),
+    radius= 0.8,cex = 1.3,main = "frequency of cancer diagnosis",border = "black")
 
-#corrrelation plot
+legend(1.1,0.2, legend = diagnosis.prop.df[,2],cex = 0.7, 
+       fill = c("purple","red"),border = "black")
+
+#correlation plot
 #calculate collineraity
 c <- cor(cancer[,3:31])
 corrplot(c, order = "hclust", tl.cex = 0.7)
 
 #comparing the radius, area concavity of begnin and malignant stage
-ggplot(cancer, aes(x=diagnosis, y=radius_mean,fill="pink"))+geom_boxplot(fill="yellow")+ggtitle( "radius_mean ofbegnin")
-ggplot(cancer, aes(x=diagnosis, y=area_mean,fill="pink"))=geom_boxplot()+ggtitle("area of begnin vs malignant")
-same for concavity,
-#barplot
-ggplot(cancer, aes(x=diagnosis, fill = texture_mean, fill = "yellow"))+geom_bar()+ggtitle("women affected in begnin and malignant")
+ggplot(cancer, aes(x=diagnosis, y=radius_mean,fill="pink"))+
+  geom_boxplot(fill="yellow")+ggtitle( "radius_mean ofbegnin")
 
-#women affected at higher level based on meam from the analysis of boxplot
+ggplot(cancer, aes(x=diagnosis, y=area_mean,fill="pink"))=geom_boxplot()+
+  ggtitle("area of begnin vs malignant")
+#same for concavity(in place of area_mean)
+
+#barplot
+ggplot(cancer, aes(x=diagnosis, fill = texture_mean, fill = "yellow"))+
+  geom_bar()+ggtitle("women affected in begnin and malignant")
+
+#women affected at higher level based on mean from the analysis of boxplot
 sel_data<- cancer[cancer$radius_mean]>10&cancer$radius_mean<15&cancer$compactness_mean>0.1
 
 
 #density plot based on texture and mean
-ggplot(cancer, aes(x=texture_mean,fill=as.factor(diagnosis)))+geom_density()+ggtitle("texture mean")
+ggplot(cancer, aes(x=texture_mean,fill=as.factor(diagnosis)))+
+  geom_density()+ggtitle("texture mean")
 
 #barplot for area_se 15
-ggplot(cancer, aes(x=area_se>15, fill = diagnosis))+ geom_bar(position = "fill")+ggtitle("area for begnin vs malignant tumour")
+ggplot(cancer, aes(x=area_se>15, fill = diagnosis))+ 
+  geom_bar(position = "fill")+ggtitle("area for begnin vs malignant tumour")
 
 #ditribution of data via histogram
-ggplot(cancer, aes(x=concavity_mean,fill=diagnosis))+geom_histogram(binwidth = 5)+ggtitle("concavity mean")
-ggplot(cancer, aes(x= texture_se)) + facet_wrap(~ diagnosis)+ggtitle("texture se for beginin vs malignant")
-ggplot(cancer, aes(x = perimeter_mean))+geom_histogram(binwidth = 15)+ facet_wrap(~ diagnosis)+ggtitle("perimeter mean for begnin and malignant")
+ggplot(cancer, aes(x=concavity_mean,fill=diagnosis))+
+  geom_histogram(binwidth = 5)+ggtitle("concavity mean")
+
+ggplot(cancer, aes(x= texture_se)) + facet_wrap(~ diagnosis)+
+  ggtitle("texture se for beginin vs malignant")
+
+ggplot(cancer, aes(x = perimeter_mean))+
+  geom_histogram(fill="yellow",col = "red",binwidth = 15)+
+  facet_wrap(~ diagnosis)+ggtitle("perimeter mean for begnin and malignant")
 
 # split the data into trainng and testing sets(logistic regression)
 cancer$diagnosis <- factor(cancer$diagnosis,levels = c("B","M"),labels = c(0,1))
